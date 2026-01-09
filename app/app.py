ab61a8f5d0cf34704e4fe88ad5ccd2e919adbe28
Ubuntu <ubuntu@ip-172-31-44-117.eu-north-1.compute.internal>
2026-01-09 09:03:44 +0000

    break app to test rollback

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Working Successfully 🚀 .   Check Now. AUTO BUILD WORKING 🚀"
-
+#    return "CI/CD Pipeline Working Successfully 🚀 .   Check Now. AUTO BUILD WORKING 🚀 . we break pods"
+     return 1/0
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
